e46f50764d99ca8630a19eba81824c204838bbfe
jeff <chenjf@idowe.com>
2023-11-08 19:08:27 +0800

    使用vite的ssr进行渲染

---
+++ packages/remix-vue-server-runtime/server.js
@@ -0,0 +1,329 @@
+
+
+
+export const createRequestHandler = (
+    build,
+    mode
+  ) => {
+    let _build;
+    let routes = [];
+    let serverMode;
+    let staticHandler;
+    let errorHandler;
+  
+    return async function requestHandler(
+      request,
+      loadContext = {},
+      { __criticalCss: criticalCss } = {}
+    ) {
+      _build = typeof build === "function" ? await build() : build;
+      if (typeof build === "function") {
+        let derived = derive(_build, mode);
+        routes = derived.routes;
+        serverMode = derived.serverMode;
+        staticHandler = derived.staticHandler;
+        errorHandler = derived.errorHandler;
+      } else if (!routes || !serverMode || !staticHandler || !errorHandler) {
+        let derived = derive(_build, mode);
+        routes = derived.routes;
+        serverMode = derived.serverMode;
+        staticHandler = derived.staticHandler;
+        errorHandler = derived.errorHandler;
+      }
+  
+      let url = new URL(request.url);
+  
+      let matches = matchServerRoutes(routes, url.pathname);
+      let handleError = (error) =>
+        errorHandler(error, {
+          context: loadContext,
+          params: matches && matches.length > 0 ? matches[0].params : {},
+          request,
+        });
+  
+      let response;
+      if (url.searchParams.has("_data")) {
+        let routeId = url.searchParams.get("_data");
+  
+        response = await handleDataRequestRR(
+          serverMode,
+          staticHandler,
+          routeId,
+          request,
+          loadContext,
+          handleError
+        );
+  
+        if (_build.entry.module.handleDataRequest) {
+          response = await _build.entry.module.handleDataRequest(response, {
+            context: loadContext,
+            params: matches?.find((m) => m.route.id == routeId)?.params || {},
+            request,
+          });
+        }
+      } else if (
+        matches &&
+        matches[matches.length - 1].route.module.default == null &&
+        matches[matches.length - 1].route.module.ErrorBoundary == null
+      ) {
+        response = await handleResourceRequestRR(
+          serverMode,
+          staticHandler,
+          matches.slice(-1)[0].route.id,
+          request,
+          loadContext,
+          handleError
+        );
+      } else {
+        response = await handleDocumentRequestRR(
+          serverMode,
+          _build,
+          staticHandler,
+          request,
+          loadContext,
+          handleError,
+          criticalCss
+        );
+      }
+  
+      if (request.method === "HEAD") {
+        return new Response(null, {
+          headers: response.headers,
+          status: response.status,
+          statusText: response.statusText,
+        });
+      }
+  
+      return response;
+    };
+  };
+
+  
+async function handleDataRequestRR(
+    serverMode,
+    staticHandler,
+    routeId,
+    request,
+    loadContext,
+    handleError
+  ) {
+    try {
+      let response = await staticHandler.queryRoute(request, {
+        routeId,
+        requestContext: loadContext,
+      });
+  
+      if (isRedirectResponse(response)) {
+        // We don't have any way to prevent a fetch request from following
+        // redirects. So we use the `X-Remix-Redirect` header to indicate the
+        // next URL, and then "follow" the redirect manually on the client.
+        let headers = new Headers(response.headers);
+        headers.set("X-Remix-Redirect", headers.get("Location"));
+        headers.set("X-Remix-Status", response.status);
+        headers.delete("Location");
+        if (response.headers.get("Set-Cookie") !== null) {
+          headers.set("X-Remix-Revalidate", "yes");
+        }
+  
+        return new Response(null, {
+          status: 204,
+          headers,
+        });
+      }
+  
+      if (DEFERRED_SYMBOL in response) {
+        let deferredData = response[DEFERRED_SYMBOL];
+        let body = createDeferredReadableStream(
+          deferredData,
+          request.signal,
+          serverMode
+        );
+        let init = deferredData.init || {};
+        let headers = new Headers(init.headers);
+        headers.set("Content-Type", "text/remix-deferred");
+        // Mark successful responses with a header so we can identify in-flight
+        // network errors that are missing this header
+        headers.set("X-Remix-Response", "yes");
+        init.headers = headers;
+        return new Response(body, init);
+      }
+  
+      // Mark all successful responses with a header so we can identify in-flight
+      // network errors that are missing this header
+      response.headers.set("X-Remix-Response", "yes");
+      return response;
+    } catch (error) {
+      if (isResponse(error)) {
+        error.headers.set("X-Remix-Catch", "yes");
+        return error;
+      }
+  
+      if (isRouteErrorResponse(error)) {
+        if (error) {
+          handleError(error);
+        }
+        return errorResponseToJson(error, serverMode);
+      }
+  
+      let errorInstance =
+        error instanceof Error ? error : new Error("Unexpected Server Error");
+      handleError(errorInstance);
+      return routerJson(serializeError(errorInstance, serverMode), {
+        status: 500,
+        headers: {
+          "X-Remix-Error": "yes",
+        },
+      });
+    }
+  }
+  
+  async function handleDocumentRequestRR(
+    serverMode,
+    build,
+    staticHandler,
+    request,
+    loadContext,
+    handleError,
+    criticalCss
+  ) {
+    let context;
+    try {
+      context = await staticHandler.query(request, {
+        requestContext: loadContext,
+      });
+    } catch (error) {
+      handleError(error);
+      return new Response(null, { status: 500 });
+    }
+  
+    if (isResponse(context)) {
+      return context;
+    }
+  
+    // Sanitize errors outside of development environments
+    if (context.errors) {
+      Object.values(context.errors).forEach((err) => {
+        // @ts-expect-error This is "private" from users but intended for internal use
+        if (!isRouteErrorResponse(err) || err.error) {
+          handleError(err);
+        }
+      });
+      context.errors = sanitizeErrors(context.errors, serverMode);
+    }
+  
+    let headers = getDocumentHeadersRR(build, context);
+  
+    let entryContext = {
+      manifest: build.assets,
+      routeModules: createEntryRouteModules(build.routes),
+      staticHandlerContext: context,
+      criticalCss,
+      serverHandoffString: createServerHandoffString({
+        url: context.location.pathname,
+        criticalCss,
+        state: {
+          loaderData: context.loaderData,
+          actionData: context.actionData,
+          errors: serializeErrors(context.errors, serverMode),
+        },
+        future: build.future,
+      }),
+      future: build.future,
+      serializeError: (err) => serializeError(err, serverMode),
+    };
+  
+    let handleDocumentRequestFunction = build.entry.module.default;
+    try {
+      return await handleDocumentRequestFunction(
+        request,
+        context.statusCode,
+        headers,
+        entryContext,
+        loadContext
+      );
+    } catch (error) {
+      handleError(error);
+  
+      // Get a new StaticHandlerContext that contains the error at the right boundary
+      context = getStaticContextFromError(
+        staticHandler.dataRoutes,
+        context,
+        error
+      );
+  
+      // Sanitize errors outside of development environments
+      if (context.errors) {
+        context.errors = sanitizeErrors(context.errors, serverMode);
+      }
+  
+      // Update entryContext for the second render pass
+      entryContext = {
+        ...entryContext,
+        staticHandlerContext: context,
+        serverHandoffString: createServerHandoffString({
+          url: context.location.pathname,
+          state: {
+            loaderData: context.loaderData,
+            actionData: context.actionData,
+            errors: serializeErrors(context.errors, serverMode),
+          },
+          future: build.future,
+        }),
+      };
+  
+      try {
+        return await handleDocumentRequestFunction(
+          request,
+          context.statusCode,
+          headers,
+          entryContext,
+          loadContext
+        );
+      } catch (error) {
+        handleError(error);
+        return returnLastResortErrorResponse(error, serverMode);
+      }
+    }
+  }
+  
+  async function handleResourceRequestRR(
+    serverMode,
+    staticHandler,
+    routeId,
+    request,
+    loadContext,
+    handleError
+  ) {
+    try {
+      // Note we keep the routeId here to align with the Remix handling of
+      // resource routes which doesn't take ?index into account and just takes
+      // the leaf match
+      let response = await staticHandler.queryRoute(request, {
+        routeId,
+        requestContext: loadContext,
+      });
+      // callRouteLoader/callRouteAction always return responses
+      invariant(
+        isResponse(response),
+        "Expected a Response to be returned from queryRoute"
+      );
+      return response;
+    } catch (error) {
+      if (isResponse(error)) {
+        // Note: Not functionally required but ensures that our response headers
+        // match identically to what Remix returns
+        error.headers.set("X-Remix-Catch", "yes");
+        return error;
+      }
+  
+      if (isRouteErrorResponse(error)) {
+        if (error) {
+          handleError(error);
+        }
+        return errorResponseToJson(error, serverMode);
+      }
+  
+      handleError(error);
+      return returnLastResortErrorResponse(error, serverMode);
+    }
+  }
